e2ef02d199a37a5469e5a3e1d92512a758090f0f
DAMA1011 <oldguy911011@gmail.com>
2022-10-03 10:05:14 +0800

    2022/10/03 school

--- PythonHome/Homework/ex_2_9.py
+++ PythonHome/Homework/ex_2_9.py
@@ -6,12 +6,12 @@
 # ****       **     *******
 # *****       *    *********
 
-n = 5 # 在此調整層數
+n = 10 # 在此調整層數
 
 for i in range(n): # 圖形 1
     i += 1
     j = 0
-    while j < i:
+    while j < i: # 製作星星
         j += 1
         print("*", end="")
     print("")
@@ -20,14 +20,24 @@ print("---------------------")
 
 for i in range(n): # 圖形 2
     j = 0
-    while j < i:
+    while j < i: # 製作空白
         j += 1
         print(" ", end="")
     j = 0
-    while (j + i) < n:
+    while (j + i) < n: # 製作星星
         j += 1
         print("*", end="")
     print("")
 
 print("---------------------")
 
+for i in range(n): # 圖形 3
+    j = i
+    while j < (n - 1): # 製作空白
+        j += 1
+        print(" ", end="")
+    j = 0
+    while j < (2*i + 1): # 製作星星
+        j += 1
+        print("*", end="")
+    print("")
